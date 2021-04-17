c3b9644f9053d3c99f0c42d00aa0031cf35bf698
chankibiswas <biswaschanki@gmail.com>
2021-04-17 20:15:57 +0530

    Longest common prefix

---
+++ src/main/java/com/example/algorithm/string/LongestPrefix.java
@@ -0,0 +1,31 @@
+package com.example.algorithm.string;
+
+public class LongestPrefix {
+
+    /*
+    Get the longest common prefix in array of strings
+     */
+    public String longestCommonPrefix(final String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        } else if (strs.length == 1) {
+            return strs[0];
+        }
+        boolean breakCondition = false;
+        int count = 0;
+        for (int i = 0; i < strs[0].length(); i++) {
+            final char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (i >= strs[j].length() || strs[j].charAt(i) != c) {
+                    breakCondition = true;
+                    break;
+                }
+            }
+            if (breakCondition) {
+                break;
+            }
+            ++count;
+        }
+        return strs[0].substring(0, count);
+    }
+}
