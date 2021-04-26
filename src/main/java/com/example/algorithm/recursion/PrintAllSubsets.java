5d03dd9b933fde5e60cd476459ba5d8a4d842814
chankibiswas <biswaschanki@gmail.com>
2021-04-26 18:10:57 +0530

    program to print all unique subsequences

--- src/main/java/com/example/algorithm/recursion/PrintAllSubsets.java
+++ src/main/java/com/example/algorithm/recursion/PrintAllSubsets.java
@@ -1,6 +1,11 @@
 package com.example.algorithm.recursion;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class PrintAllSubsets {
+
+    private final Set<String> op = new HashSet<>();
 
     /*
     Prints all subsets of a String input
@@ -14,5 +19,22 @@ public class PrintAllSubsets {
         }
         printAllSubSets(input.substring(1), output);
         printAllSubSets(input.substring(1), output + input.charAt(0));
+    }
+
+    /*
+    Prints all unique subsets of a String input
+    input - aaab
+    i.e., when there are repeated characters
+     */
+    private void printAllUniqueSubSets(final String input, final String output) {
+        if (input.length() == 0) {
+            if (!op.contains(output)) {
+                System.out.println(output);
+                op.add(output);
+            }
+            return;
+        }
+        printAllUniqueSubSets(input.substring(1), output);
+        printAllUniqueSubSets(input.substring(1), output + input.charAt(0));
     }
 }
