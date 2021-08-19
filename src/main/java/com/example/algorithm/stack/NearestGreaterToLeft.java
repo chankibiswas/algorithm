3b97885e2f93abb7191ccdd3ede0ee4b095ab242
chankibiswas <biswaschanki@gmail.com>
2021-08-19 09:42:21 +0530

    Nearest greater to the Left

---
+++ src/main/java/com/example/algorithm/stack/NearestGreaterToLeft.java
@@ -0,0 +1,40 @@
+package com.example.algorithm.stack;
+
+import java.util.Stack;
+
+/*
+Given an array of integers, find the closest (not considering distance, but value) greater on left of every element.
+If an element has no greater on the left side, print -1.
+ */
+public class NearestGreaterToLeft {
+
+    public static void main(final String[] s) {
+        final int[] arr = {1, 3, 1, 0, 0, 2, 4};
+        new NearestGreaterToLeft().printNearestGreaterToLeft(arr);
+    }
+
+    private void printNearestGreaterToLeft(final int[] arr) {
+        final Stack<Integer> stack = new Stack<>();
+        final int[] ans = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            if (stack.isEmpty()) {
+                ans[i] = -1;
+            } else if (!stack.isEmpty() && stack.peek() > arr[i]) {
+                ans[i] = stack.peek();
+            } else if (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                    stack.pop();
+                }
+                if (stack.isEmpty()) {
+                    ans[i] = -1;
+                } else {
+                    ans[i] = stack.peek();
+                }
+            }
+            stack.push(arr[i]);
+        }
+        for (int i = 0; i < ans.length; i++) {
+            System.out.println(ans[i]);
+        }
+    }
+}
