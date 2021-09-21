7add704dfc994c9676a7257e7aaeb6bc74507706
chankibiswas <biswaschanki@gmail.com>
2021-09-21 23:43:36 +0530

    changed some algorithms

--- src/main/java/com/example/algorithm/arrays/NextPermutation.java
+++ src/main/java/com/example/algorithm/arrays/NextPermutation.java
@@ -6,18 +6,24 @@ public class NextPermutation {
         Time complexity: O(n)
         Space complexity: O(1)
         -------------------------
+        Example - 1 3 5 4 2 -> 1 4 2 3 5
+        -------------------------
         Algorithm:
-        1. Compare nums[i] & nums[i-1]
-        2. For i-1, find the next highest number's index
-        3. Swap i-1 and next highest number index values
-        4. Reverse the array now from index i
+        1. Traverse from the end and compare nums[i-1]>=nums[i]. This index will be the pivot.
+            1 3 5 4 2 -  3 is the pivot
+        2. Keeping the pivot, find the next highest number's index to pivot on the right side to it.
+            1 3 5 4 2 - 4 is the next highest to 3
+        3. Swap pivot and next highest number index values
+            1 4 5 3 2
+        4. Reverse the array now after pivot index
+            1 4 2 3 5
      */
     public void nextPermutation(final int[] nums) {
         if (nums.length == 1) {
             return;
         }
         int i = nums.length - 1;
-        while (i > 0 && nums[i] <= nums[i - 1]) {
+        while (i > 0 && nums[i - 1] >= nums[i]) {
             i--;
         }
         if (i > 0) {
--- src/main/java/com/example/algorithm/recursion/PermutationInString.java
+++ src/main/java/com/example/algorithm/recursion/PermutationInString.java
@@ -2,27 +2,18 @@ package com.example.algorithm.recursion;
 
 public class PermutationInString {
 
-    /*
-    Normal Case where all characters are to be used find all possible permutations
-    Input - ABC
-    Output - ABC, ACB, BAC, BCA, CAB, CBA
-     */
-    public void permutationInString(String input, final int index) {
-        if (index == input.length() - 1) {
-            System.out.println(input);
-            return;
-        }
-        for (int i = index; i < input.length(); i++) {
-            input = swap(input, index, i);
-            permutationInString(input, index + 1);
-        }
+    public static void main(final String[] s) {
+        permutationInString("abc", "");
     }
 
-    private String swap(final String input, final int a, final int b) {
-        final char[] chars = input.toCharArray();
-        final char temp = chars[a];
-        chars[a] = chars[b];
-        chars[b] = temp;
-        return String.valueOf(chars);
+    public static void permutationInString(final String input, final String output) {
+        if (input.length() == 0) {
+            System.out.println(output);
+            return;
+        }
+        for (int i = 0; i < input.length(); i++) {
+            final String newInput = input.substring(0, i) + input.substring(i + 1);
+            permutationInString(newInput, output + input.charAt(i));
+        }
     }
 }
