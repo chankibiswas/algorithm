525c2edc31f787494e0d2e9d1f40d02023eba550
chankibiswas <biswaschanki@gmail.com>
2021-10-23 20:33:07 +0530

    Queue using Stack

---
+++ src/main/java/com/example/algorithm/stack/ImplementQueueUsingStacks.java
@@ -0,0 +1,65 @@
+package com.example.algorithm.stack;
+
+import java.util.Stack;
+
+/*
+Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions
+of a normal queue (push, peek, pop, and empty).
+
+Implement the MyQueue class:
+
+void push(int x) Pushes element x to the back of the queue.
+int pop() Removes the element from the front of the queue and returns it.
+int peek() Returns the element at the front of the queue.
+boolean empty() Returns true if the queue is empty, false otherwise.
+Notes:
+
+You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty
+operations are valid.
+Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque
+(double-ended queue) as long as you use only a stack's standard operations.
+*
+Input
+["MyQueue", "push", "push", "peek", "pop", "empty"]
+[[], [1], [2], [], [], []]
+Output
+[null, null, null, 1, 1, false]
+ */
+public class ImplementQueueUsingStacks {
+
+    private final Stack<Integer> s1;
+    private final Stack<Integer> s2;
+    private int front;
+
+    public ImplementQueueUsingStacks() {
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+    }
+
+    public void push(final int x) {
+        if (s1.isEmpty()) {
+            front = x;
+        }
+        s1.push(x);
+    }
+
+    public int pop() {
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+        return s2.pop();
+    }
+
+    public int peek() {
+        if (!s2.isEmpty()) {
+            return s2.peek();
+        }
+        return front;
+    }
+
+    public boolean empty() {
+        return s1.isEmpty() && s2.isEmpty();
+    }
+}
